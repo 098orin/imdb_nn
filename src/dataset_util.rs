a95d910062fa6634a0aecef961f0619d49c2760a
098orin <098orin0102@gmail.com>
2025-12-27 01:31:04 +0900

    BatchIterを作成

---
+++ src/dataset_util.rs
@@ -0,0 +1,22 @@
+pub struct BatchIter<I> {
+    iter: I,
+    batch_size: usize,
+}
+
+impl<I> BatchIter<I> {
+    pub fn new(iter: I, batch_size: usize) -> Self {
+        Self { iter, batch_size }
+    }
+}
+
+impl<I> Iterator for BatchIter<I>
+where
+    I: Iterator,
+{
+    type Item = Vec<I::Item>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let batch: Vec<_> = self.iter.by_ref().take(self.batch_size).collect();
+        if batch.is_empty() { None } else { Some(batch) }
+    }
+}
