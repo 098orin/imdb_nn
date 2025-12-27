013aded3ec6a9af69ac840b52e339a6caa6cf0d5
098orin <098orin0102@gmail.com>
2025-12-27 13:05:23 +0900

    使用しないメソッドを削除

--- src/dataset_util.rs
+++ src/dataset_util.rs
@@ -1,8 +1,6 @@
 pub struct BatchIter<I> {
     iter: I,
     batch_size: usize,
-    index: usize,
-    max_size: Option<usize>,
 }
 
 impl<I> BatchIter<I>
@@ -13,18 +11,7 @@ where
         Self {
             iter,
             batch_size,
-            index: 0,
-            max_size: None,
         }
-    }
-
-    pub fn set_max_size(mut self, max_size: usize) -> Self {
-        self.max_size = Some(max_size);
-        self
-    }
-
-    pub fn take_skipping(&mut self, u: usize) {
-        let _ = self.iter.by_ref().take(u);
     }
 }
 
@@ -35,12 +22,6 @@ where
     type Item = Vec<I::Item>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.index += self.batch_size;
-        if let Some(max_size) = self.max_size
-            && max_size + self.batch_size <= self.index
-        {
-            return None;
-        }
         let batch: Vec<_> = self.iter.by_ref().take(self.batch_size).collect();
         if batch.is_empty() { None } else { Some(batch) }
     }
