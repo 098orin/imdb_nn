e5e79d39ced146e83cf13df7e74695cf00f1b7c7
098orin <098orin0102@gmail.com>
2025-12-28 16:54:16 +0900

    Chain<M, N>に学習を実装

--- src/nn.rs
+++ src/nn.rs
@@ -116,6 +116,37 @@ where
     }
 }
 
+impl<M, N> Chain<M, N>
+where
+    Self: Module<Output = Dense>,
+{
+    pub fn train_batch<L: Loss>(
+        &mut self,
+        xs: &[<Self as Module>::Input],
+        ys: &[usize],
+        loss: &mut L,
+        lr: f32,
+    ) {
+        let batch_size = xs.len();
+
+        for (x, &y) in xs.iter().zip(ys.iter()) {
+            // forward
+            let mut y_pred = <Self as Module>::Output::zeros_like_input(x);
+            self.forward(x, &mut y_pred);
+
+            // loss backward
+            let mut grad_y = y_pred.zeros_like();
+            loss.backward(&y_pred, y, &mut grad_y);
+
+            // backward
+            let mut grad_x = x.zeros_like();
+            self.backward(&grad_y, x, &mut grad_x);
+        }
+
+        self.step(lr, batch_size);
+    }
+}
+
 #[allow(dead_code)]
 pub trait Loss {
     /// 損失値を計算（評価用）
